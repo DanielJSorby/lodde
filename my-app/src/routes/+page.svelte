754cc130619c0fde790fe2f14ded9dcea826ae5d
Daniel Johan Sorby <daniel@superandreas.com>
2024-06-18 13:39:53 +0200

    lagde basic knapper, og splash page

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -5,16 +5,54 @@
 
 <body>
 	<Navbar darkmode={true}/>
-	<header>
-		<h1>Hei</h1>
-	</header>
+	<div class="start">
+		<div class="start-things">
+			<div class="header">
+				<h1>Lodde</h1>
+			</div>
+			<div class="info">
+				<p>Norges nettbasserte<br>Loddsystem</p>
+			</div>
+			<div class="buttons">
+				<button class="kig background blue">Kom i gang</button>
+				<button class="fum transparent">Finn ut mer</button>
+			</div>
+		</div>
+	</div>
 </body>
 
 <style>
-	body {
+	.start {
+		height: 100vh;
+		width: 100%;
 		background-color: #E94F37;
+		color: white;
+		border-radius: 0 0 80px 80px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
 	}
-	header {
-		height: 300vh;
+
+	.start h1 {
+		font-size: 300px;
+		margin-bottom: -90px;
+	}
+
+	.start p {
+		font-size: 40px;
+	}
+
+	.start button {
+		font-size: 30px;
+		padding: 10px 20px;
+		border-radius: 20px;
+		margin: 10px;
+	}
+
+	.start-things {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
 	}
 </style>
